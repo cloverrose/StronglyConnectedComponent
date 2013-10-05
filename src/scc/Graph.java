49d61ea19f59d0ec8533f38268d27d9b0881a58a
cloverrose <clover.plus.rose@gmail.com>
2013-10-06 00:10:15 +0900

    alpha

---
+++ src/scc/Graph.java
@@ -0,0 +1,24 @@
+package scc;
+import java.util.*;
+public class Graph<T> {
+	private Map<T, Collection<T>> edges;
+
+	public Graph(){
+		this.edges = new HashMap<T, Collection<T>>();  // fromVertex -> [toVertex1,...,toVertexN]
+	}
+
+	public Collection<T> getVertexes(){
+		return this.edges.keySet();
+	}
+
+	public Collection<T> getAdjacentVertexes(T vertex){
+		return this.edges.get(vertex);
+	}
+
+	public void addEdge(T src, T dst){
+		if(!this.edges.containsKey(src)){
+			this.edges.put(src, new ArrayList<T>());
+		}
+		this.edges.get(src).add(dst);
+	}
+}

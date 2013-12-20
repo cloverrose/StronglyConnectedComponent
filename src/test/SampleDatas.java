3a4ad11d7b86b141858ccfe495ceba1ec87b777e
cloverrose <a_osari@worksap.co.jp>
2013-12-20 17:00:34 +0900

    refactor test; at least one edge outgo

--- src/test/SampleDatas.java
+++ src/test/SampleDatas.java
@@ -7,7 +7,7 @@ public class SampleDatas {
         Graph<Integer> graph = new Graph<Integer>(numOfVertexes);
         Random rnd = new Random();
         for(int src=0;src<numOfVertexes;src++){
-            for(int j=0;j<rnd.nextInt(maxNumOfAdjacentVertexes + 1);j++){
+            for(int j=0;j<rnd.nextInt(maxNumOfAdjacentVertexes) + 1;j++){
                 graph.addEdge(src, rnd.nextInt(numOfVertexes));
             }
         }
